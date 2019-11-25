16df0c4a77a117ff03d8e4c83db20f9fef4efc98
Muammer <muammeray52@icloud.com>
2019-11-25 11:43:55 +0300

    Initial Commit

---
+++ ilkoyun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ilkoyun
+//
+//  Created by Muammer  on 25.11.2019.
+//  Copyright © 2019 Muammer . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
